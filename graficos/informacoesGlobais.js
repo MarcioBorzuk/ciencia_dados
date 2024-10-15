9368927c421f0bc5f8303c940feb311ac04c1f37
Kleber <127142110+Prof-Kleber@users.noreply.github.com>
2024-10-15 19:47:45 +0000

    inclui arquivo js

---
+++ graficos/informacoesGlobais.js
@@ -0,0 +1,14 @@
+const url='https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
+
+
+async function vizualizarInformacoesGlobais() {
+
+    const resposta = await fetch(url)
+
+    const dados = await resposta.json()
+
+    console.log(dados)
+
+}
+
+vizualizarInformacoesGlobais()
