3fb1f56f0eb17e7a3ae2b41d274d7e6ca930087f
Kleber <127142110+Prof-Kleber@users.noreply.github.com>
2024-10-22 19:46:53 +0000

    ajustado

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -12,8 +12,8 @@ async function vizualizarInformacoesGlobais() {
 
 
 const paragrafo= document.createElement('p')
-paragrafo.classList.add(graficos-container__texto)
-paragrafo.innerHTML = `Você sabia que no mundo tem ${dados.total_pessoas_mundo} de pessoas e que aproximadamente ${dados.total_pessoas_conctadas} estão conectadas em alguma rede social ${dados.tempo_medio} horas conectadas`
+paragrafo.classList.add('graficos-container__texto')
+paragrafo.innerHTML = `Você sabia que no mundo tem ${dados.total_pessoas_mundo} de pessoas e que aproximadamente ${dados.total_pessoas_conectadas} estão conectadas em alguma rede social ${dados.tempo_medio} horas conectadas`
 const container=document.getElementById('graficos-container')
     container.appendChild(paragrafo);
 
