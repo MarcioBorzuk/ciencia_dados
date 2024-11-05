78b941ad2e373660764e99b8448d4bdaba7d0fcb
Kleber <127142110+Prof-Kleber@users.noreply.github.com>
2024-11-05 19:49:12 +0000

    aula 6

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -1,4 +1,4 @@
-const url='https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
+const url='https://raw.githubusercontent.com/silviosnjr/CienciaDeDados-CriandoGraficosDinamicosComJavaScript/refs/heads/Aula01/transporte/transporte-dados-globais.json'
 
 
 async function vizualizarInformacoesGlobais() {
@@ -7,13 +7,17 @@ async function vizualizarInformacoesGlobais() {
 
     const dados = await resposta.json()
 
-    //console.log(dados)
-
+    const pessoasMundo = (dados.total_pessoas_mundo/1e9)
+    const trabalhadoresMundo = (dados.total_trabalhadores_mundo/1e9)
+    const tempoDesTrabalho = parseInt (dados.tempo_medio_deslocamento_para_trabalho)
 
 
 const paragrafo= document.createElement('p')
+
 paragrafo.classList.add('graficos-container__texto')
-paragrafo.innerHTML = `Você sabia que no mundo tem <span> ${dados.total_pessoas_mundo} </span> de pessoas e que aproximadamente <span> ${dados.total_pessoas_conectadas}</span> estão conectadas em alguma rede social <span>${dados.tempo_medio}</span> horas conectadas`
+
+paragrafo.innerHTML = `O mundo tem <span>${pessoasMundo}</span>  bilhões de pessoas, dessas pessoas, aproximadamente <span>${trabalhadoresMundo}</span>   bilhões estão empregadas e passam em média <span>${tempoDesTrabalho}</span> horas por dia trabalhando. Temos, portanto, mais da metade da população mundial que não exerce trabalhos com vínculos empregatícios legais. O que pode significar que há longevidade e desse modo, muitas pessoas aposentadas. Porém, também pode significar muitos postos de trabalho chamados informais.`
+
 const container=document.getElementById('graficos-container')
     container.appendChild(paragrafo);
 
